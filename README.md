e85ccf0328bba3902fd794869361607babb9d75f
arunesh <nysdas@icloud.com>
2022-06-26 08:01:10 -0400

    add Setup the API v1 along with project data files
    url: /api/v1/tours
    This will help us to build and test v2 without breaking the
    system for version 1.
    
    File systsem files will be read once and must be outside the
    event loop function.
    
    It is better to send status code with each request.
    API GET Request
    	localhost:3000/api/v1/tours
    API GET Response
    	Status
    	length of the array
    	Data from JSON file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # wiP
+
+# Section 6 Express: 
+
+Express is a framework for building web applications and APIs. It is designed to be fast, flexible, and powerful.
+
+## Starting our API: Handling GET requests
+
+
+
+We are sending JSON formatted data as our response.
+
--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const express = require(`express`);
 
 const app = express();
 
+// this cannot be inside the event loop function.
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+
+app.get(`/api/v1/tours`, (req, res) => {
+    // When we have status of 200 (OK)
+    res.status(200).json({
+        status: `success`,      // When user is successfully able to connect with the API
+        results: tours.length, // length of the array sent to frontend
+        data:{
+            // tours is the const tours from JSON file from directory
+            tours
+        }
+    })
+})
 // Port number
 const port = 3000;
 
