b8d668e72290e9b393454d96e1173c96793ba708
arunesh <nysdas@icloud.com>
2022-06-30 20:20:36 -0400

    Refractored the APIFEATURES- filter, SORT, PAGINATION
    Earlier everything was configured under the getAllTours
    Now, we could use all these features in any other routes.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,6 @@
 const fs = require(`fs`);
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
 const Tour = require(`./../models/tourModel`);
 
 exports.aliasTopTours = (req, res, next) => {
@@ -8,90 +10,109 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 }
 
-exports.getAllTours = async (req, res) => {
-  console.log(req.query);
-  try {
-    // BUILD QUERY
-    // destructure the query
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const tours = await features.query;
 
-    // 1. FILTERING
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
 
-    // 2. ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
 
-    // without g it will filter only first occurence
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match =>  `$${match}`);
-    console.log(JSON.parse(queryStr))
+// exports.getAllTours = async (req, res) => {
+//   console.log(req.query);
+//   try {
+//     // BUILD QUERY
+//     // destructure the query
+
+//     // 1. FILTERING
+//     const queryObj = { ...req.query };
+//     const excludedFields = ['page', 'sort', 'limit', 'fields'];
+//     excludedFields.forEach((el) => delete queryObj[el]);
+
+//     // 2. ADVANCED FILTERING
+//     let queryStr = JSON.stringify(queryObj);
+
+//     // without g it will filter only first occurence
+//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match =>  `$${match}`);
+//     console.log(JSON.parse(queryStr))
 
 
      
-    // const query = Tour.find(queryObj);
-    // to make sure gte or lte are taken care of use this
-    let query = Tour.find(JSON.parse(queryStr));
-    // without any query it will just send  all as output
-    // with valid query it will send the filtered output
+//     // const query = Tour.find(queryObj);
+//     // to make sure gte or lte are taken care of use this
+//     let query = Tour.find(JSON.parse(queryStr));
+//     // without any query it will just send  all as output
+//     // with valid query it will send the filtered output
 
-    // SORTING
-    if(req.query.sort){
-      const sortBy = req.query.sort.split(`,`).join(` `);
-      console.log(sortBy)
-      query = query.sort(sortBy);
-    }else{
-      // query = query.sort(`-createdAt`);
-      query = query.sort(`price`);
-    }
+//     // SORTING
+//     if(req.query.sort){
+//       const sortBy = req.query.sort.split(`,`).join(` `);
+//       console.log(sortBy)
+//       query = query.sort(sortBy);
+//     }else{
+//       // query = query.sort(`-createdAt`);
+//       query = query.sort(`price`);
+//     }
 
-    // 3 Field limiting
+//     // 3 Field limiting
 
-    if(req.query.fields){
-      const fields = req.query.fields.split(',').join('  ');
-      //projecting
-      query = query.select(fields)
-    }else {
-      // excluding only this __v which is used by mongoose internally
-      query = query.select(`-__v`);
-    }
+//     if(req.query.fields){
+//       const fields = req.query.fields.split(',').join('  ');
+//       //projecting
+//       query = query.select(fields)
+//     }else {
+//       // excluding only this __v which is used by mongoose internally
+//       query = query.select(`-__v`);
+//     }
 
-    // 4 pagination
+//     // 4 pagination
 
-    // pages=2&limit=10
+//     // pages=2&limit=10
 
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page -1) * limit;
+//     const page = req.query.page * 1 || 1;
+//     const limit = req.query.limit * 1 || 100;
+//     const skip = (page -1) * limit;
 
-    query = query.skip(skip).limit(limit);
+//     query = query.skip(skip).limit(limit);
 
-    if(req.query.page){
-      const numTours = await Tour.countDocuments();
+//     if(req.query.page){
+//       const numTours = await Tour.countDocuments();
 
-      if(skip >= numTours) throw new Error(`This page does not exist`);
-    }
+//       if(skip >= numTours) throw new Error(`This page does not exist`);
+//     }
 
-    // EXECUTE Query
-    const tours = await query;
+//     // EXECUTE Query
+//     const tours = await query;
    
     
 
     
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      result: tours.length,
-      data: {
-        tours,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+//     // SEND RESPONSE
+//     res.status(200).json({
+//       status: 'success',
+//       result: tours.length,
+//       data: {
+//         tours,
+//       },
+//     });
+//   } catch (err) {
+//     res.status(404).json({
+//       status: 'fail',
+//       message: err,
+//     });
+//   }
+// };
 
 exports.getTour = async (req, res) => {
   console.log(req.params.id);
